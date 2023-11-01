439c2598943a8e55e226812ed60e7b6d47c09d04
viniciusgcf <149544283+viniciusgcf@users.noreply.github.com>
2023-11-01 08:41:08 -0300

    Create cache.sh
    limpa cache do chrome e firefox

---
+++ cache.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf /home/$SUDO_USER/.cache/google-chrome/Default/Cache
+rm -rf /home/$SUDO_USER/.cache/google-chrome/Default/Media\Cache
+rm -rf /home/$SUDO_USER/.cache/google-chrome/Profile\ 2/Cache
+
+rm -rf /home/$SUDO_USER/.cache/mozilla/firefox
